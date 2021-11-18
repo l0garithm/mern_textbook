a11b3acbc4614d6b8951f62618f9167648bef44f
Logan - Sanctuary <lacarlson5@gmail.com>
2021-11-18 05:16:50 +0000

    test

---
+++ basics/webpack.config.client.production.js
@@ -0,0 +1,24 @@
+const path = require("path/posix");
+
+const config = {
+    mode: "production",
+    entry: [
+        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+    ],
+    output: {
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
+        filename: 'bundle.js',
+        publicPath: "/dist/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            }
+        ]
+    }
+}
